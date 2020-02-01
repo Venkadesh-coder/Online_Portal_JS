1f017bd07f0d5f19b1efd3cb935dab1ac8323e5a
Venkadesh-coder <54761484+Venkadesh-coder@users.noreply.github.com>
2020-02-01 23:10:37 +0530

    Add files via upload

---
+++ subjects.js
@@ -0,0 +1,55 @@
+function awp()
+{
+window.open("awp.html");
+}
+
+function esd()
+{
+window.open("esd.html");
+}
+
+function cc()
+{
+window.open("cc.html");
+}
+
+function wc()
+{
+window.open("wc.html");
+}
+
+function ae()
+{
+window.open("ae.html");
+}
+
+function ne()
+{
+window.open("ne.html");
+}
+
+function ias()
+{
+window.open("ias.html");
+}
+
+function iot()
+{
+window.open("iot.html");
+}
+
+function ce_lab_2()
+{
+window.open("ce_lab_2.html");
+}
+
+function vlsi_lab()
+{
+window.open("vlsi_lab.html");
+}
+
+function esd_lab()
+{
+window.open("esd_lab.html");
+}
+
